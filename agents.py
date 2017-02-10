b3650ab8d7d30e6996a7eb9d8a400957e86a8f9d
Sergei Korolev <slipped.on.blade@gmail.com>
2017-02-10 16:33:36 +0300

    kek

--- agents.py
+++ agents.py
@@ -10,12 +10,35 @@ from gym.spaces.box import Box
 from universe import vectorized
 from universe.wrappers import  Unvectorize, Vectorize
 
+import numpy as np
+import tensorflow as tf
+import os
+import sys
+import re
+import time
+import random
+import argparse
+import six
+import cv2
+
+from tensorpack import *
+from tensorpack.RL import *
+from common import play_one_episode
+
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 NUM_EPISODES =  5
 CHECKPOINT_LOCATION = '/home/ubuntu/pacman/train'
 MONITOR_LOCATION = "./pacman-1"
+
+IMAGE_SIZE = (84, 84)
+FRAME_HISTORY = 4
+CHANNEL = FRAME_HISTORY * 3
+IMAGE_SHAPE3 = IMAGE_SIZE + (CHANNEL,)
+
+NUM_ACTIONS = None
+ENV_NAME = None
 
 #
 # This was copy-pasted from openai/universe-starter-agent
@@ -161,7 +184,7 @@ class A3C(object):
             c_in = g.get_tensor_by_name("global/Placeholder_1:0")
             h_in = g.get_tensor_by_name("global/Placeholder_2:0")
 
-            # 
+            #
             # print('Trainable vars in {}:'.format(tf.get_variable_scope().name))
             # for v in var_list:
             #     print('  %s %s', v.name, v.get_shape())
@@ -181,7 +204,7 @@ class A3C(object):
                 terminal = False
                 while not terminal:
                     feed_dict = {inp : obs[np.newaxis],
-                                c_in: last_state[0], 
+                                c_in: last_state[0],
                                 h_in: last_state[1]}
 
                     state, sampled_action = sess.run([get_output_state, get_sample_op], feed_dict = feed_dict)
@@ -216,3 +239,92 @@ class RandomAgent(object):
                     break
             rewards.append(total)
         return sum(rewards)/float(len(rewards))
+
+
+class TBAgent(object):
+    def __init__(self, env, NUM_EPISODES):
+        self.env = env
+        self.NUM_EPISODES = NUM_EPISODES
+
+        ENV_NAME = self.env
+        assert ENV_NAME
+        logger.info("Environment Name: {}".format(ENV_NAME))
+        p = self.get_player()
+        del p    # set NUM_ACTIONS
+
+        os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+
+        self.cfg = PredictConfig(
+            model=Model(),
+            session_init=SaverRestore(),
+            input_names=['state'],
+            output_names=['logits'])
+        # run_submission(self.cfg, args.output, args.episode)
+
+    def get_player(dumpdir=None):
+        pl = GymEnv(self.env, dumpdir=dumpdir, auto_restart=False)
+        pl = MapPlayerState(pl, lambda img: cv2.resize(img, IMAGE_SIZE[::-1]))
+
+        global NUM_ACTIONS
+        NUM_ACTIONS = pl.get_action_space().num_actions()
+
+        pl = HistoryFramePlayer(pl, FRAME_HISTORY)
+        return pl
+
+
+    class Model(ModelDesc):
+        def _get_inputs(self):
+            assert NUM_ACTIONS is not None
+            return [InputVar(tf.float32, (None,) + IMAGE_SHAPE3, 'state'),
+                    InputVar(tf.int32, (None,), 'action'),
+                    InputVar(tf.float32, (None,), 'futurereward')]
+
+        def _get_NN_prediction(self, image):
+            image = image / 255.0
+            with argscope(Conv2D, nl=tf.nn.relu):
+                l = Conv2D('conv0', image, out_channel=32, kernel_shape=5)
+                l = MaxPooling('pool0', l, 2)
+                l = Conv2D('conv1', l, out_channel=32, kernel_shape=5)
+                l = MaxPooling('pool1', l, 2)
+                l = Conv2D('conv2', l, out_channel=64, kernel_shape=4)
+                l = MaxPooling('pool2', l, 2)
+                l = Conv2D('conv3', l, out_channel=64, kernel_shape=3)
+
+            l = FullyConnected('fc0', l, 512, nl=tf.identity)
+            l = PReLU('prelu', l)
+            policy = FullyConnected('fc-pi', l, out_dim=NUM_ACTIONS, nl=tf.identity)
+            return policy
+
+        def _build_graph(self, inputs):
+            state, action, futurereward = inputs
+            policy = self._get_NN_prediction(state)
+            self.logits = tf.nn.softmax(policy, name='logits')
+
+
+    def run_submission(self, cfg, output=None, nr=self.NUM_EPISODES):
+        player = get_player(dumpdir=None)
+        predfunc = get_predict_func(cfg)
+        logger.info("Start evaluation: ")
+        for k in range(self.NUM_EPISODES):
+            if k != 0:
+                player.restart_episode()
+            score = play_one_episode(player, predfunc)
+            print("Score:", score)
+
+
+    def do_submit(self, output):
+        gym.upload(output, api_key='xxx')
+
+
+    def play(self):
+        player = self.get_player(dumpdir=None)
+        predfunc = self.get_predict_func(self.cfg)
+        rewards = []
+        # logger.info("Start evaluation: ")
+        for k in range(self.NUM_EPISODES):
+            if k != 0:
+                player.restart_episode()
+            score = play_one_episode(player, predfunc)
+            rewards.append(score)
+        return sum(rewards)/float(len(rewards))
+
