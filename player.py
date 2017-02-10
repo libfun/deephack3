24d87ceb878b36ff66c3418d4980e761c0cca4c1
Sergei Korolev <slipped.on.blade@gmail.com>
2017-02-10 21:31:36 +0300

    newpalyer

--- player.py
+++ player.py
@@ -1,22 +1,20 @@
 import numpy as np
 
+
 class Player(object):
 
-	def __init__(self, env, agents):
-		self.env = env
-		self.agents = {agent.name: agent for agent in agents}
-		self.best_agent = None
+    def __init__(self, env, agents):
+        self.env = env
+        self.agents = {agent.name: agent for agent in agents}
+        self.best_agent = None
 
-	def choose_and_record(self, num_episodes, self.env):
+    def choose_and_record(self, num_episodes):
+        scores = {}
+        for agent in self.agents.keys:
+            scores[agent] = self.agents[agent].play(num_episodes,
+                                                    env=self.env,
+                                                    record=False)
 
-		scores = {}
-
-		for agent in self.agents.keys:
-			scores[agent] = self.agents[agent].play(num_episodes, record = False)
-
-		self.agents[max(scores, key=scores.get)].play(num_episodes, record = True)
-
-
-
-
-
+        self.agents[max(scores, key=scores.get)].play(num_episodes,
+                                                      env=self.env,
+                                                      record=True)
