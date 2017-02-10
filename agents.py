6d9ba7ec968581cb5419479240d721c2e4e46a0a
Sergey Sviridov <sergei.sviridov@gmail.com>
2017-02-10 15:48:09 +0300

    added bandit classes

---
+++ agents.py
@@ -0,0 +1,218 @@
+import time
+import logging
+import numpy as np
+import cv2
+import go_vncdriver
+import tensorflow as tf
+import gym
+from gym import wrappers
+from gym.spaces.box import Box
+from universe import vectorized
+from universe.wrappers import  Unvectorize, Vectorize
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+NUM_EPISODES =  5
+CHECKPOINT_LOCATION = '/home/ubuntu/pacman/train'
+MONITOR_LOCATION = "./pacman-1"
+
+#
+# This was copy-pasted from openai/universe-starter-agent
+#
+def DiagnosticsInfo(env, *args, **kwargs):
+    return vectorized.VectorizeFilter(env, DiagnosticsInfoI, *args, **kwargs)
+
+class DiagnosticsInfoI(vectorized.Filter):
+    def __init__(self, log_interval=503):
+        super(DiagnosticsInfoI, self).__init__()
+
+        self._episode_time = time.time()
+        self._last_time = time.time()
+        self._local_t = 0
+        self._log_interval = log_interval
+        self._episode_reward = 0
+        self._episode_length = 0
+        self._all_rewards = []
+        self._num_vnc_updates = 0
+        self._last_episode_id = -1
+
+    def _after_reset(self, observation):
+        logger.info('Resetting environment')
+        self._episode_reward = 0
+        self._episode_length = 0
+        self._all_rewards = []
+        return observation
+
+    def _after_step(self, observation, reward, done, info):
+        to_log = {}
+        if self._episode_length == 0:
+            self._episode_time = time.time()
+
+        self._local_t += 1
+        if info.get("stats.vnc.updates.n") is not None:
+            self._num_vnc_updates += info.get("stats.vnc.updates.n")
+
+        if self._local_t % self._log_interval == 0:
+            cur_time = time.time()
+            elapsed = cur_time - self._last_time
+            fps = self._log_interval / elapsed
+            self._last_time = cur_time
+            cur_episode_id = info.get('vectorized.episode_id', 0)
+            to_log["diagnostics/fps"] = fps
+            if self._last_episode_id == cur_episode_id:
+                to_log["diagnostics/fps_within_episode"] = fps
+            self._last_episode_id = cur_episode_id
+            if info.get("stats.gauges.diagnostics.lag.action") is not None:
+                to_log["diagnostics/action_lag_lb"] = info["stats.gauges.diagnostics.lag.action"][0]
+                to_log["diagnostics/action_lag_ub"] = info["stats.gauges.diagnostics.lag.action"][1]
+            if info.get("reward.count") is not None:
+                to_log["diagnostics/reward_count"] = info["reward.count"]
+            if info.get("stats.gauges.diagnostics.clock_skew") is not None:
+                to_log["diagnostics/clock_skew_lb"] = info["stats.gauges.diagnostics.clock_skew"][0]
+                to_log["diagnostics/clock_skew_ub"] = info["stats.gauges.diagnostics.clock_skew"][1]
+            if info.get("stats.gauges.diagnostics.lag.observation") is not None:
+                to_log["diagnostics/observation_lag_lb"] = info["stats.gauges.diagnostics.lag.observation"][0]
+                to_log["diagnostics/observation_lag_ub"] = info["stats.gauges.diagnostics.lag.observation"][1]
+
+            if info.get("stats.vnc.updates.n") is not None:
+                to_log["diagnostics/vnc_updates_n"] = info["stats.vnc.updates.n"]
+                to_log["diagnostics/vnc_updates_n_ps"] = self._num_vnc_updates / elapsed
+                self._num_vnc_updates = 0
+            if info.get("stats.vnc.updates.bytes") is not None:
+                to_log["diagnostics/vnc_updates_bytes"] = info["stats.vnc.updates.bytes"]
+            if info.get("stats.vnc.updates.pixels") is not None:
+                to_log["diagnostics/vnc_updates_pixels"] = info["stats.vnc.updates.pixels"]
+            if info.get("stats.vnc.updates.rectangles") is not None:
+                to_log["diagnostics/vnc_updates_rectangles"] = info["stats.vnc.updates.rectangles"]
+            if info.get("env_status.state_id") is not None:
+                to_log["diagnostics/env_state_id"] = info["env_status.state_id"]
+
+        if reward is not None:
+            self._episode_reward += reward
+            if observation is not None:
+                self._episode_length += 1
+            self._all_rewards.append(reward)
+
+        if done:
+            logger.info('Episode terminating: episode_reward=%s episode_length=%s', self._episode_reward, self._episode_length)
+            total_time = time.time() - self._episode_time
+            to_log["global/episode_reward"] = self._episode_reward
+            to_log["global/episode_length"] = self._episode_length
+            to_log["global/episode_time"] = total_time
+            to_log["global/reward_per_time"] = self._episode_reward / total_time
+            self._episode_reward = 0
+            self._episode_length = 0
+            self._all_rewards = []
+
+        return observation, reward, done, to_log
+
+def _process_frame42(frame):
+    #frame = frame[34:34+160, :160]
+    # Resize by half, then down to 42x42 (essentially mipmapping). If
+    # we resize directly we lose pixels that, when mapped to 42x42,
+    # aren't close enough to the pixel boundary.
+    frame = cv2.resize(frame, (80, 80))
+    frame = cv2.resize(frame, (42, 42))
+    #frame = frame.mean(2)
+    frame = frame.astype(np.float32)
+    frame *= (1.0 / 255.0)
+    frame = np.reshape(frame, [42, 42, 3])
+    return frame
+
+class AtariRescale42x42(vectorized.ObservationWrapper):
+    def __init__(self, env=None):
+        super(AtariRescale42x42, self).__init__(env)
+        self.observation_space = Box(0.0, 1.0, [42, 42, 3])
+
+    def _observation(self, observation_n):
+        return [_process_frame42(observation) for observation in observation_n]
+
+
+def create_atari_env(env_id):
+    env = gym.make(env_id)
+    env = Vectorize(env)
+    env = AtariRescale42x42(env)
+    env = DiagnosticsInfo(env)
+    env = Unvectorize(env)
+    return env
+
+class A3C(object):
+
+    def __init__(self, env, MONITOR_LOCATION, CHECKPOINT_LOCATION, NUM_EPISODES):
+
+        self.env = env
+        self.MONITOR_LOCATION = MONITOR_LOCATION
+        self.chkpt = tf.train.latest_checkpoint(CHECKPOINT_LOCATION)
+        self.NUM_EPISODES = NUM_EPISODES
+        # logger.info("Loading checkpoint {}".format(chkpt))
+
+    def play(self):
+        with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)) as sess, sess.as_default():
+            saver = tf.train.import_meta_graph(self.chkpt + ".meta", clear_devices=True)
+            g = tf.get_default_graph()
+            saver.restore(sess, self.chkpt)
+            state_out_0 = tf.get_collection("state_out_0")[0]
+            state_out_1 = tf.get_collection("state_out_1")[0]
+            get_sample_op = tf.get_collection("greedy_action")[0]
+            get_output_state = [state_out_0, state_out_1]
+
+            inp = g.get_tensor_by_name("global/Placeholder:0")
+            c_in = g.get_tensor_by_name("global/Placeholder_1:0")
+            h_in = g.get_tensor_by_name("global/Placeholder_2:0")
+
+            # 
+            # print('Trainable vars in {}:'.format(tf.get_variable_scope().name))
+            # for v in var_list:
+            #     print('  %s %s', v.name, v.get_shape())
+            # for tensor in tf.get_default_graph().as_graph_def().node:
+            #     print(tensor.name)
+
+            lengths = []
+            rewards = []
+            for ep in range(self.NUM_EPISODES):
+                obs = env.reset()
+
+                initial_state = np.zeros((1,256)).astype(float)
+                last_state = [initial_state, initial_state]
+
+                length = 0
+                reward_sum = 0
+                terminal = False
+                while not terminal:
+                    feed_dict = {inp : obs[np.newaxis],
+                                c_in: last_state[0], 
+                                h_in: last_state[1]}
+
+                    state, sampled_action = sess.run([get_output_state, get_sample_op], feed_dict = feed_dict)
+                    action = sampled_action #.argmax()
+                    obs, reward, terminal, info = env.step(action)
+
+                    last_state = state
+                    length += 1
+                    reward_sum += reward
+                    # print("Episode finished in {} reward: {}".format(length, rewards))
+                lengths.append(length)
+                rewards.append(reward_sum)
+            return sum(rewards)/float(len(rewards))
+
+class RandomAgent(object):
+
+    def __init__(self, env, NUM_EPISODES):
+        self.env = env
+        self.NUM_EPISODES = NUM_EPISODES
+
+    def play(self, ):
+        env = gym.make(env)
+        rewards = []
+        for i_episode in range(num_episodes):
+            observation = env.reset()
+            total = 0
+            for t in range(10000):
+                action = env.action_space.sample()
+                observation, reward, done, info = env.step(action)
+                total += reward
+                if done:
+                    break
+            rewards.append(total)
+        return sum(rewards)/float(len(rewards))
---
+++ bandit.py
@@ -0,0 +1,166 @@
+class MultiArmedBandit(object):
+    """
+    A Multi-armed Bandit
+    """
+    def __init__(self, agents):
+        self.k = len(agents)
+        self.agents = agents
+
+    def pull(self, agent, num_episodes):
+        return agent.play(num_episodes)
+        
+
+class Player(object):
+    """
+    A Player is able to play one of a set of agents at each trial. The
+    agent is chosen using a strategy based on the history of prior plays
+    and outcome observations.
+    """
+    def __init__(self, bandit, policy, prior=0, gamma=None):
+        self.policy = policy
+        self.k = bandit.k
+        self.prior = prior
+        self.gamma = gamma
+        self._reward_estimates = prior*np.ones(self.k)
+        self.agent_attempts = np.zeros(self.k)
+        self.t = 0
+        self.last_agent = None
+
+    def __str__(self):
+        return 'f/{}'.format(str(self.policy))
+
+    def reset(self):
+        """
+        Resets the player's memory to an initial state.
+        """
+        self._reward_estimates[:] = self.prior
+        self.agent_attempts[:] = 0
+        self.last_agent = None
+        self.t = 0
+
+    def choose(self):
+        agent = self.policy.choose(self)
+        self.last_agent = agent
+        return agent
+
+    def observe(self, reward):
+        self.agent_attempts[self.last_agent] += 1
+
+        if self.gamma is None:
+            g = 1 / self.agent_attempts[self.last_agent]
+        else:
+            g = self.gamma
+        q = self._reward_estimates[self.last_agent]
+
+        self._reward_estimates[self.last_agent] += g*(reward - q)
+        self.t += 1
+
+    @property
+    def reward_estimates(self):
+        return self._reward_estimates
+
+
+class Environment(object):
+    def __init__(self, bandit, player, num_episodes, label='Multi-Armed Bandit'):
+        self.bandit = bandit
+        self.player = player
+        self.label = label
+
+    def reset(self):
+        self.player.reset()
+
+    def run(self, trials=100, experiments=1):
+        scores = np.zeros((trials, 1))
+
+        for _ in range(experiments):
+            self.reset()
+            for t in range(trials):
+                agent = player.choose()
+                reward = self.bandit.pull(agent, num_episodes)
+                player.observe(reward)
+
+        return scores / experiments, self.player._reward_estimates
+
+
+    """
+    def plot_results(self, scores):
+        sns.set_style('white')
+        sns.set_context('talk')
+        plt.subplot(2, 1, 1)
+        plt.title(self.label)
+        plt.plot(scores)
+        plt.ylabel('Average Reward')
+        plt.legend(self.player, loc=4)
+        plt.subplot(2, 1, 2)
+        plt.plot(optimal * 100)
+        plt.ylim(0, 100)
+        plt.ylabel('% Optimal Action')
+        plt.xlabel('Time Step')
+        plt.legend(self.player, loc=4)
+        sns.despine()
+        plt.show()
+    """
+
+        
+
+class Policy(object):
+    """
+    A policy prescribes an agent to be chosen based on the memory of an player.
+    """
+    def __str__(self):
+        return 'generic policy'
+
+    def choose(self, player):
+        return 0
+
+class EpsilonGreedyPolicy(Policy):
+    """
+    The Epsilon-Greedy policy will choose a random agent with probability
+    epsilon and run the best apparent agent with probability 1-epsilon. If
+    multiple agents are tied for best choice, then a random agent from that
+    subset is selected.
+    """
+    def __init__(self, epsilon):
+        self.epsilon = epsilon
+
+    def __str__(self):
+        return '\u03B5-greedy (\u03B5={})'.format(self.epsilon)
+
+    def choose(self, player):
+        if np.random.random() < self.epsilon:
+            return np.random.choice(len(player.reward_estimates))
+        else:
+            agent = np.argmax(player.reward_estimates)
+            check = np.where(player.reward_estimates == agent)[0]
+            if len(check) == 0:
+                return agent
+            else:
+                return np.random.choice(check)
+
+class UCBPolicy(Policy):
+    """
+    The Upper Confidence Bound algorithm (UCB1). It applies an exploration
+    factor to the expected value of each agent which can influence a greedy
+    selection strategy to more intelligently explore less confident options.
+    """
+    def __init__(self, c):
+        self.c = c
+
+    def __str__(self):
+        return 'UCB (c={})'.format(self.c)
+
+    def choose(self, agent):
+        exploration = np.log(player.t+1) / player.action_attempts
+        exploration[np.isnan(exploration)] = 0
+        exploration = np.power(exploration, 1/self.c)
+
+        q = player.value_estimates + exploration
+        agent = np.argmax(q)
+        check = np.where(q == agent)[0]
+        if len(check) == 0:
+            return agent
+        else:
+            return np.random.choice(check)
+
+
+
